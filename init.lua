b8e911d184b813a8fbba328c5acb8976ea5e3b4e
Ethan <ethanamaher@gmail.com>
2025-05-05 21:10:12 -0500

    added preview and ability to enter into better diff view with <CR>

--- init.lua
+++ init.lua
@@ -9,6 +9,8 @@ local finders = require("telescope.finders")
 -- local conf  = require("telescope.config").values
 local sorters = require("telescope.sorters")
 local previewers = require("telescope.previewers")
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
 
 local function get_visual_selection()
     local _, start_row, _, _ = unpack(vim.fn.getpos("'<"))
@@ -81,7 +83,7 @@ function M.investigate_selection()
     end
 
     local log_range = string.format("%d,%d", selection.start_line, selection.end_line)
-    local format_arg = "--format=%h %ad %an %s"
+    local format_arg = "--format=%H %ad %an %s"
     local date_arg = "--date=short"
     local range_arg = "-L" .. log_range .. ":" .. rel_file_path
 
@@ -107,19 +109,131 @@ function M.investigate_selection()
                 return
             end
 
+            local git_previewer = previewers.new_buffer_previewer {
+                define_preview = function(self, entry, status)
+                    if not vim.api.nvim_buf_is_valid(self.state.bufnr) then
+                        return
+                    end
+
+                    -- set buffer modifiable and clear it
+                    vim.api.nvim_buf_set_option(self.state.bufnr, "modifiable", true)
+                    vim.api.nvim_buf_set_option(self.state.bufnr, "readonly", false)
+
+                    -- clear buffer
+                    vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, {})
+
+                    if not entry or not entry.value or not entry.value.hash then
+                        local err_msg = "Error invalid entry"
+                        vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, { err_msg, vim.inspect(entry)})
+                        vim.api.nvim_buf_set_option(self.state.bufnr, "readonly", true)
+                        vim.api.nvim_buf_set_option(self.state.bufnr, "modifiable", false)
+                        return
+                    end
+
+                    local commit_hash = entry.value.hash
+                    local show_cmd_args = {"show", commit_hash}
+
+                    local output_lines = {}
+                    local error_lines = {}
+                    local job_exit_code = -1
+
+                    Job:new({
+                        command = "git",
+                        args = show_cmd_args,
+                        cwd = repo_root,
+
+                        on_stdout = function(_, data)
+                            if data then table.insert(output_lines, data) end
+                        end,
+
+                        on_stderr = function(_, data)
+                            if data then table.insert(error_lines, data) end
+                        end,
+
+                        on_exit = vim.schedule_wrap(function(_, code)
+                            job_exit_code = code
+
+                            if not vim.api.nvim_buf_is_valid(self.state.bufnr) then return end
+
+                            local final_content = {}
+
+                            if job_exit_code ~= 0 then
+
+                                final_content = error_lines
+                                if #final_content == 0 then
+                                    table.insert(final_content, "Failed")
+                                end
+
+                            else
+                                final_content = output_lines
+                                if #final_content == 0 then
+                                    table.insert(final_content, "Failed")
+                                end
+                            end
+                            vim.api.nvim_buf_set_option(self.state.bufnr, "modifiable", true)
+                            vim.api.nvim_buf_set_option(self.state.bufnr, "readonly", false)
+
+                            vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, final_content)
+                            vim.api.nvim_buf_set_option(self.state.bufnr, "filetype", "git")
+
+
+                            vim.api.nvim_buf_set_option(self.state.bufnr, "readonly", true)
+                            vim.api.nvim_buf_set_option(self.state.bufnr, "modifiable", false)
+
+                        end),
+                    }):start()
+                end,
+            }
+
             pickers.new({}, {
                 prompt_title = "Code Block History",
                 finder = finders.new_table({
                     results = commit_list,
                     entry_maker = function(entry)
+                        local trimmed_hash = string.sub(entry.hash, 1, 7)
+
                         return {
                             value = entry,
-                            display = string.format("%s (%s) | %s | %s", entry.hash, entry.date, entry.author, entry.subject),
+                            display = string.format("%s (%s) | %s | %s", trimmed_hash, entry.date, entry.author, entry.subject),
                             ordinal = entry.date .. " " .. entry.hash,
                         }
                     end
                 }),
                 sorter = sorters.get_generic_fuzzy_sorter({}),
+                previewer = git_previewer,
+                layout_strategy = 'horizontal',
+                layout_config = {
+                    horizontal = {
+                        preview_width = 0.5,
+                    }
+                },
+                attach_mappings = function(prompt_bufnr, map)
+                    -- local current_picker = action_state.get_current_picker(prompt_bufnr)
+
+                    actions.select_default:replace(function()
+                        local sel = action_state.get_selected_entry()
+                        actions.close(prompt_bufnr)
+
+                        if sel and sel.value and sel.value.hash then
+                            local hash = sel.value.hash
+                            local file = sel.filename
+
+                            if vim.fn.exists(":Gvdiffsplit") == 2 then
+                                vim.cmd("Gvdiffsplit " .. hash)
+                            elseif vim.fn.exists(":DiffviewOpen") == 2 then
+                                vim.cmd("DiffviewOpen " .. hash .. ".." .. hash .. " -- " .. file)
+                            else
+                                if vim.fn.exists(":Git") == 2 then
+                                    vim.cmd("tab Git show " .. hash)
+                                else
+                                    vim.cmd("tabnew | term git -C " .. vim.fn.shellescape(repo_root) .. " show " .. hash)
+                                end
+                            end
+                        end
+                    end)
+
+                    return true
+                end
             }):find()
         end)
     })
@@ -132,7 +246,7 @@ end
 function M.setup()
     vim.api.nvim_create_user_command("BlameInvestigate", M.investigate_selection, {
         range = true,
-        desc = "Invesigate Git history of selected code block",
+        desc = "Investigate Git history of selected code block",
     })
 end
 
